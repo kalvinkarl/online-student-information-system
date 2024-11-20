0d834b1a6db2146fafedc851358cd3ade9f66636
kalvinkarl <kalvinkarl28@outlook.com>
2024-11-20 16:29:25 +0800

    Initialize server

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "csusolana-session",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true
+  })
+);
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to csusolana application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
